2f82911851205cef2123e612671cc27c5a9fb8f8
OHelmyy <omar.helmy.re@gmail.com>
2026-04-05 00:05:08 +0200

    introduce template method design pattern and pipeline-based workflow architecture

---
+++ apps/analysis/pipelines/__init__.py
@@ -0,0 +1,11 @@
+from .base_pipeline import BasePipeline
+from .predev_pipeline import PreDevPipeline
+from .postdev_pipeline import PostDevPipeline
+from .recommendation_pipeline import RecommendationPipeline
+
+__all__ = [
+    "BasePipeline",
+    "PreDevPipeline",
+    "PostDevPipeline",
+    "RecommendationPipeline",
+]
