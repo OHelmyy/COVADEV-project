bf1af9f6fe742c0ba5cc496d0c943f1708d9e11b
bassell01 <basselmohsen041@gmail.com>
2026-04-04 17:16:12 +0200

    remove legacy routing logic and defer routing to router provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,3 @@
-// frontend/src/App.tsx
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
-
-import ProjectsPage from "./pages/ProjectsPage";
-import ProjectDetailPage from "./features/projects/pages/ProjectDetailPage";
-
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Default */}
-        <Route path="/" element={<Navigate to="/projects" replace />} />
-
-        {/* Main */}
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
-
-        {/* Reports */}
-       {/* <Route path="/reports" element={<ReportsPage />} />*/}
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/projects" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return null;
 }
