698023e7660b551ce1649fadbdbd741420189756
bassell01 <basselmohsen041@gmail.com>
2026-04-08 15:28:00 +0200

    Update urls.py

--- apps/analysis/urls.py
+++ apps/analysis/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path("api/analysis/<int:project_id>/metrics/summary/", views.metrics_summary, name="metrics_summary"),
     path("api/analysis/<int:project_id>/metrics/details/", views.metrics_details, name="metrics_details"),
     path("api/analysis/<int:project_id>/metrics/developers/", views.metrics_developers, name="metrics_developers"),
-path("api/reports/dashboard/", views.dashboard_stats, name="dashboard_stats"),
-path("api/reports/dashboard/", views.dashboard_stats, name="dashboard_stats"),
+    path("api/reports/dashboard/", views.dashboard_stats, name="dashboard_stats"),
+    
 
     path("api/analysis/<int:project_id>/compare-inputs/", views.compare_inputs_api, name="compare_inputs_api"),
     #path("run/", views.run_analysis, name="run_analysis"),
